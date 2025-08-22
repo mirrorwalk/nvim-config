48ce6368aa2a507e53b2c136e37f158af033e7af
mirrorwalk <git.cresting327@passmail.net>
2025-08-23 00:19:33 +0200

    Backup: 2025-08-23 00:19:33

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,22 @@
 return {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
+    {
+        "folke/tokyonight.nvim",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            -- load the colorscheme here
+            require('tokyonight').setup({ transparent = true })
+            -- vim.cmd([[colorscheme tokyonight]])
+        end,
+    },
+    {
+        "scottmckendry/cyberdream.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- load the colorscheme here
+            require('cyberdream').setup({ transparent = true })
+            vim.cmd([[colorscheme cyberdream]])
+        end,
+    }
 }
