8208ae5447a4cf5a5608ddb7b94b39b17f3efe73
mirrorwalk <git.cresting327@passmail.net>
2025-08-29 20:20:33 +0200

    Backup: 2025-08-29 20:20:33

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,3 +5,19 @@ vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end, {})
 vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
+vim.keymap.set('n', '<leader>f', builtin.current_buffer_fuzzy_find, { noremap = true, silent = true })
+
+require('telescope').setup {
+  defaults = {
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+    },
+    layout_strategy = 'horizontal',
+  },
+}
