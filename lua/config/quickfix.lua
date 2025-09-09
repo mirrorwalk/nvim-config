69e6a4532a8e0096d99ee9ada7d00d00f16e973b
mirrorwalk <git.cresting327@passmail.net>
2025-09-09 20:08:34 +0200

    Backup: 2025-09-09 20:08:34

---
+++ lua/config/quickfix.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+vim.keymap.set("n", "<M-c>", "<cmd>cclose<CR>")
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,33 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-    vim.keymap.set('n', '<leader>ps', function()
-      builtin.grep_string({ search = vim.fn.input("Grep > ") })
-    end, {})
-    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<CR>', {})
-    vim.keymap.set('n', '<leader>fz', builtin.current_buffer_fuzzy_find, { noremap = true, silent = true })
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
+    config = function()
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        -- vim.keymap.set('n', '<leader>ps', function()
+        --     builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        -- end, {})
+        vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<CR>', {})
+        vim.keymap.set('n', '<leader>fz', builtin.current_buffer_fuzzy_find, { noremap = true, silent = true })
 
-    require('telescope').setup {
-      defaults = {
-        vimgrep_arguments = {
-          'rg',
-          '--color=never',
-          '--no-heading',
-          '--with-filename',
-          '--line-number',
-          '--column',
-          '--smart-case',
-        },
-        layout_strategy = 'horizontal',
-      },
-    }
-  end,
+
+        require('telescope').setup {
+            extensions = {
+                fzf = {}
+            },
+            pickers = {
+                find_files = {
+                    theme = "ivy"
+                }
+            }
+        }
+
+        require('telescope').load_extension('fzf')
+        require "config.telescope.multigrep".setup()
+    end,
 }
