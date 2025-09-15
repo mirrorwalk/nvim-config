e2c35ec0068f286c6121338f7781edaf23ebf91c
mirrorwalk <git.cresting327@passmail.net>
2025-09-16 01:10:31 +0200

    Simplified lsp

--- lua/config/quickfix.lua
+++ lua/config/quickfix.lua
@@ -1,3 +1,4 @@
-vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
-vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+vim.keymap.set("n", "<M-n>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-p>", "<cmd>cprev<CR>")
 vim.keymap.set("n", "<M-c>", "<cmd>cclose<CR>")
+vim.keymap.set("n", "<M-o>", "<cmd>copen<CR>")
--- lua/plugins/nvim-lsp.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "mason.nvim",
-    { "williamboman/mason-lspconfig.nvim", config = function() end },
-  },
-}
