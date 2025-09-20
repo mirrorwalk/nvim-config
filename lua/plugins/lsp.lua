ee2753548ea739717dea507bb74c80c918c108b3
mirrorwalk <git.cresting327@passmail.net>
2025-09-20 06:01:07 +0200

    Backup: 2025-09-20 06:01:07

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,18 +14,18 @@ return {
         },
         config = function()
             local capabilities = require('blink.cmp').get_lsp_capabilities()
-            -- require("lspconfig").lua_ls.setup { capabilites = capabilities }
-            -- require("lspconfig").zls.setup { capabilites = capabilities }
-            -- require("lspconfig").gdscript.setup { capabilites = capabilities }
-
-            -- local servers = { 'lua_ls', 'zls', 'gdscript' }
-            --
-            -- for _, server in ipairs(servers) do
-            --     vim.lsp.config(server, { capabilities = capabilities })
-            --     vim.lsp.enable(server)
-            -- end
 
             vim.lsp.config('*', { capabilities = capabilities })
+
+            vim.lsp.config('nil_ls', {
+                settings = {
+                    ['nil'] = {
+                        formatting = {
+                            command = { 'alejandra', '-q' },
+                        },
+                    },
+                },
+            })
 
             vim.api.nvim_create_autocmd('LspAttach', {
                 callback = function(args)
